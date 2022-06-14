5e1a2bdec7c7b9de7e9149d00aee1180f7b23738
Emir Alp Elmas <90847528+EmiralpElmas@users.noreply.github.com>
2022-06-14 11:40:02 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const modulbtn=document.getElementById("submit");
+const modul=document.querySelector(".modal");
+const connectBtn = document.querySelector(".button");
+const modulkapat=document.getElementById("modal-kapat");
+const accounttxt=document.querySelector(".account");
+const publictxt=document.querySelector(".public");
+
+
+connectBtn.addEventListener("click",async ()=>{
+    var connect =  await ethereum.request({ method: 'eth_requestAccounts' });
+    var balance_adress = await connect[0];
+});
+
+modulbtn.addEventListener("click",()=>{
+    modul.style.display="flex";
+    });
